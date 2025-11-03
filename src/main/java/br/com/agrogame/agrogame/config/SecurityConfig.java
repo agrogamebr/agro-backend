da21dca77b5b675bd62077dfcf1da8181b4f2523
Matheus Diniz <matheusalvesdiniz@gmail.com>
2025-11-03 16:47:49 -0300

    update CORS config method to enable swagger

--- src/main/java/br/com/agrogame/agrogame/config/SecurityConfig.java
+++ src/main/java/br/com/agrogame/agrogame/config/SecurityConfig.java
@@ -134,10 +134,13 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
+
+        configuration.setAllowedOriginPatterns(Arrays.asList("*"));
+
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
+        configuration.setExposedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setMaxAge(3600L);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
